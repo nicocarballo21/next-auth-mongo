76c85cfe2c5395651af437c9e14049c03691d73a
Nicolas carballo <nlucas120@gmail.com>
2024-02-04 16:22:08 -0300

    adding configd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import type {ReactNode} from "react";
+
+import {Inter} from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
